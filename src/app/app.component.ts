f46650f3086a1999788450e0828491bfb2d21955
Necdet UYGUR <necdet.uygur@gmail.com>
2023-03-16 20:14:36 +0300

    Load wait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    setTimeout(() => {
+      document.body.style.opacity = '1';
+    }, 1e3);
+  }
+}
